1f51bac01800c01770ee7864c4908fec19c08f38
Raman Hundal <rshundal11@gmail.com>
2018-03-22 22:30:36 +0000

    Added comments to scripts

--- setup_mysql_dev.sql
+++ setup_mysql_dev.sql
@@ -1,4 +1,4 @@
-# This script sets up a MySQL DB and sets up new users
+-- This script sets up a MySQL DB and sets up new users
 CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
 CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
 GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
