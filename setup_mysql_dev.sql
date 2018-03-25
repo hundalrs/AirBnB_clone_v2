f43447824371ad6461721fdc49ba356b51acca03
RoMalms10 <156@holbertonschool.com>
2018-03-25 01:06:47 +0000

    Set up MySQL environment
    Create a database 'hbnb_dev_db' and create a user for it with certain privileges

---
+++ setup_mysql_dev.sql
@@ -0,0 +1,6 @@
+#!/usr/bin/env bash
+# This script sets up a MySQL DB and sets up new users
+CREATE DATABASE IF NOT EXISTS hbnb_dev_db;
+CREATE USER IF NOT EXISTS'hbnb_dev'@'localhost' IDENTIFIED BY 'hbnb_dev_pwd';
+GRANT ALL PRIVILEGES ON hbnb_dev_db.* TO 'hbnb_dev'@'localhost';
+GRANT SELECT ON performance_schema.* TO 'hbnb_dev'@'localhost';
