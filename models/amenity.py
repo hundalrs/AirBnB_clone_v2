3b7ee07eedf895fac0df95dde6ca441393b6164f
RoMalms10 <156@holbertonschool.com>
2018-03-29 00:05:11 +0000

    Update

--- models/amenity.py
+++ models/amenity.py
@@ -3,6 +3,7 @@
     Implementation of the Amenity class
 '''
 from models.base_model import BaseModel, Base
+import models
 import os
 from sqlalchemy import Column, String, Integer, ForeignKey, DateTime
 from sqlalchemy.orm import relationship
@@ -16,8 +17,10 @@ class Amenity(BaseModel, Base):
     __tablename__ = 'amenities'
 
     if os.getenv('HBNB_TYPE_STORAGE') == 'db':
+        from models.place import association_table
         name = Column(String(128), nullable=False)
 
-        place_amenities = relationship()
+        place_amenities = relationship("Place", secondary=association_table)
     else:
         name = ""
+
