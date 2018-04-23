1d0d982f254cf329d13c3308384e98a1030f1620
Raman Hundal <rshundal11@gmail.com>
2018-04-23 02:12:23 +0000

    Abide to pep8

--- web_flask/9-states.py
+++ web_flask/9-states.py
@@ -17,6 +17,7 @@ def states_html():
     return render_template('9-states.html', state_objs=state_objs,
                            state_list=None)
 
+
 @app.route('/states/<id>', strict_slashes=False)
 def state_id(id):
     '''displays states with id'''
